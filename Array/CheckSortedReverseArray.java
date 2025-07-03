671a5a78452edf7a50625fc56edd0ce4763aad77
dhakatarang <tarang.kumar.cse.2022@miet.ac.in>
2025-07-03 18:57:59 +0530

    modified

---
+++ Array/CheckSortedReverseArray.java
@@ -0,0 +1,6 @@
+public class CheckSortedReverseArray {
+  public static void main(String[] args){
+    
+  }
+  
+}
--- Array/ReverseArray.java
+++ Array/ReverseArray.java
@@ -1,3 +1,20 @@
+
+import java.util.Scanner;
 public class ReverseArray {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] arr = new int[n];
+    
+    for(int i=0 ;i<n;i++){
+      arr[i] = sc.nextInt();
+      System.out.println(arr[i]);
+    }
+
+    for(int i=n-1;i>=0;i--){
+      System.out.println(arr[i]);
+    }
+
+  }
   
 }
